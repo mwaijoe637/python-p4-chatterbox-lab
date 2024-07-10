5c4ca2e1b19134a8bd9308b6cc461fcfa5c1cc87
mwaijoe637 <brakejoe9@gmail.com>
2024-07-10 05:48:56 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from datetime import datetime
 
 from models import db, Message
 
@@ -14,13 +15,47 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    serialized_messages = [message.to_dict() for message in messages]
+    return jsonify(serialized_messages)
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['POST'])
+def create_message():
+    body = request.json.get('body')
+    username = request.json.get('username')
+    if not body or not username:
+        return make_response(jsonify({'error': 'Missing body or username'}), 400)
+
+    new_message = Message(body=body, username=username)
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get(id)
+    if not message:
+        return make_response(jsonify({'error': 'Message not found'}), 404)
+
+    new_body = request.json.get('body')
+    if new_body:
+        message.body = new_body
+        message.updated_at = datetime.utcnow()
+        db.session.commit()
+    
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get(id)
+    if not message:
+        return make_response(jsonify({'error': 'Message not found'}), 404)
+
+    db.session.delete(message)
+    db.session.commit()
+    return jsonify({'message': 'Message deleted'})
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
@@ -12,3 +13,7 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
